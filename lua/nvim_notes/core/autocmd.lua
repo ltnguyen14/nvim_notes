617f588cac998063f40d7e1ee59d71802caa8ad8
lamdr96 <lamdr96@gmail.com>
2022-05-17 16:43:38 -0400

    Open a new window to insert text - still need to clear out old values when updating notes

--- lua/nvim_notes/core/autocmd.lua
+++ lua/nvim_notes/core/autocmd.lua
@@ -1,7 +1,24 @@
-local diag = require"nvim_notes.core.diag"
+local nvim_notes = require"nvim_notes"
 
 vim.api.nvim_create_augroup("nvim_notes", {})
 vim.api.nvim_create_autocmd({"BufEnter"}, {
   callback = function()
+    require("nvim_notes").showNotesInBuffer()
   end
 })
+
+vim.api.nvim_create_user_command("NvimNotesCreateNote",
+  function (args)
+    nvim_notes.addNoteToLine(args.args)
+    nvim_notes.showNotesInBuffer()
+    nvim_notes.saveNotes()
+  end,
+  {}
+)
+
+vim.api.nvim_create_user_command("NvimNotesOpenNote",
+  function ()
+    nvim_notes.openNote()
+  end,
+  {}
+)
--- lua/nvim_notes/init.lua
+++ lua/nvim_notes/init.lua
@@ -3,6 +3,8 @@ local core = require"nvim_notes.core"
 local diag = require"nvim_notes.core.diag"
 local util = require"nvim_notes.utils"
 local notes = {}
+local current_file
+local current_line
 
 M.setup = function ()
   require("nvim_notes.core.autocmd")
@@ -33,6 +35,38 @@ M.addNoteToBuffer = function (line, note)
     notes[filePath] = {string.format("%s: %s", line, note)}
   end
 end
+
+M.addNoteToLine = function (note)
+  local filePath = vim.api.nvim_buf_get_name(0)
+  local cursorLine, cursorColumn = unpack(vim.api.nvim_win_get_cursor(0))
+  cursorLine = cursorLine - 1;
+  if (notes[filePath]) then
+    table.insert(notes[filePath], string.format("%s: %s", cursorLine, note))
+  else
+    notes[filePath] = {string.format("%s: %s", cursorLine, note)}
+  end
+end
+
+M.getNoteForLine = function ()
+  local filePath = vim.api.nvim_buf_get_name(0)
+  local cursorLine, cursorColumn = unpack(vim.api.nvim_win_get_cursor(0))
+  cursorLine = cursorLine - 1;
+  current_line = cursorLine
+  current_file = filePath
+
+  if notes[filePath] then
+    local allNotes = {}
+    for _, lineRaw in ipairs(notes[filePath]) do
+      local lineData = util.parseLineAndMsg(lineRaw)
+      if lineData and lineData.line == cursorLine then
+        table.insert(allNotes, lineData.msg)
+      end
+    end
+    return allNotes
+  end
+
+end
+
 
 M.getNotesByFile = function (filePath)
   local lineMessages = notes[filePath]
@@ -65,6 +99,28 @@ M.showNotesInBuffer = function ()
     end
     diag.createDiag(bufferNotes)
   end
+end
+
+M.openNote = function ()
+  local windowOptions = vim.lsp.util.make_floating_popup_options(40, 20)
+  windowOptions.border = "rounded"
+  local buf = vim.api.nvim_create_buf(false, true)
+  local notesForLine = M.getNoteForLine()
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, notesForLine)
+
+  vim.api.nvim_open_win(buf, true, windowOptions)
+end
+
+M.updateNoteBuffer = function ()
+  local newNotes = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+
+  if notes[current_file] then
+    for _, msg in ipairs(newNotes) do
+      table.insert(notes[current_file], string.format('%s: %s', current_line, msg))
+    end
+  end
+
+  M.saveNotes()
 end
 
 return M
